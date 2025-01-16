8a766d1624e2ae625be46b67825ee0c0c1abd567
Valentin Valencia Valencia <valentinv3v7@gmail.com>
2025-01-16 14:57:59 -0500

    first successful maps api integration

--- app/src/main/java/com/valentinConTilde/onmywayapp/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/valentinConTilde/onmywayapp/ui/main/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.valentinConTilde.onmywayapp.ui.main.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,13 +9,19 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.valentinConTilde.onmywayapp.databinding.FragmentHomeBinding
 import com.valentinConTilde.onmywayapp.io.WebSocketClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -27,53 +34,69 @@ class HomeFragment : Fragment() {
 
     private val homeViewModel: HomeViewModel by activityViewModels()
 
+    private lateinit var googleMap: GoogleMap
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        Log.d("HomeFragment", "onCreateView")
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        //textView.text = "This is home Fragment"
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        //Get the map fragment and set up the map
+        val mapFragment = childFragmentManager.findFragmentById(com.valentinConTilde.onmywayapp.R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("HomeFragment", "onViewCreated")
 
-        val messageTextView = binding.messageTextView
-        val messageInput = binding.messageInput
-        val sendButton = binding.sendButton
 
-        // Initialize WebSocketClient
-        webSocketClient = WebSocketClient()
+    }
 
-        // Connect to WebSocket
-        webSocketClient.connectToWebSocket() { message ->
-            // Handle incoming message
-            // Update UI or handle the message as needed
-            //Log.d("WebSocket", "Received message: $message")
-        }
+    override fun onResume() {
+        super.onResume()
+        Log.d("HomeFragment", "onResume")
+    }
 
-        // Handle send button click
-        sendButton.setOnClickListener {
-            val message = messageInput.text.toString()
-            lifecycleScope.launch(Dispatchers.IO) {
-                webSocketClient.sendMessage(message)
-            }
-        }
+    override fun onPause() {
+        super.onPause()
+        Log.d("HomeFragment", "onPause")
+    }
 
+    override fun onStop() {
+        super.onStop()
+        Log.d("HomeFragment", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("HomeFragment", "onDestroy")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("HomeFragment", "onStart")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        Log.d("HomeFragment", "onDestroyView")
+
         _binding = null
-        webSocketClient.closeWebSocket()  // Close WebSocket when fragment is destroyed
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+
+        // Set a default marker
+        val defaultLocation = LatLng(37.7749, -122.4194) // San Francisco coordinates
+        googleMap.addMarker(MarkerOptions().position(defaultLocation).title("San Francisco"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 12f))
     }
 }
